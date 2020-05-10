fff7a49c4f17bba9867248f6958939f165ed3254
Kesho <se.hakam88@gmail.com>
2020-05-10 03:13:58 +0200

    another solution for perfect square

--- src/main/java/com/problems/leetcode/MayChallenge/PerfectSquare.java
+++ src/main/java/com/problems/leetcode/MayChallenge/PerfectSquare.java
@@ -9,28 +9,35 @@ public class PerfectSquare
 		// System.out.println(isPerfectSquare(2));
 		// System.out.println(isPerfectSquare(3));
 		// System.out.println(isPerfectSquare(4));
-		System.out.println(isPerfectSquare(16));
+		System.out.println(isPerfectSquare(14));
 	}
 
 	public static boolean isPerfectSquare(int num)
 	{
-		if(num < 0)
-			return false;
-		if(num == 0)
-			return true;
-		long left = 1;
-		long right = (long) Integer.MAX_VALUE;
-		while(left <= right)
+		// if(num < 0)
+		// return false;
+		// if(num == 0)
+		// return true;
+		// long left = 1;
+		// long right = (long) Integer.MAX_VALUE;
+		// while(left <= right)
+		// {
+		// long mid = left + right >>> 1;
+		// long midSqr = mid * mid;
+		// if(midSqr == num)
+		// return true;
+		// else if(midSqr > num)
+		// right = mid - 1;
+		// else
+		// left = mid + 1;
+		// }
+		// return false;
+
+		long guess = num;
+		while(guess * guess > num)
 		{
-			long mid = left + right >>> 1;
-			long midSqr = mid * mid;
-			if(midSqr == num)
-				return true;
-			else if(midSqr > num)
-				right = mid - 1;
-			else
-				left = mid + 1;
+			guess = (guess + num / guess) >>> 1;
 		}
-		return false;
+		return guess * guess == num;
 	}
 }
