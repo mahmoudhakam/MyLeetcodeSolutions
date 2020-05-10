0a61f88f15d29971b46237dd496423455e2d85e8
Kesho <se.hakam88@gmail.com>
2020-05-10 03:07:18 +0200

    adding perfect square problem

---
+++ src/main/java/com/problems/leetcode/MayChallenge/PerfectSquare.java
@@ -0,0 +1,36 @@
+package com.problems.leetcode.MayChallenge;
+
+public class PerfectSquare
+{
+	public static void main(String[] args)
+	{
+		// System.out.println(isPerfectSquare(0));
+		// System.out.println(isPerfectSquare(1));
+		// System.out.println(isPerfectSquare(2));
+		// System.out.println(isPerfectSquare(3));
+		// System.out.println(isPerfectSquare(4));
+		System.out.println(isPerfectSquare(16));
+	}
+
+	public static boolean isPerfectSquare(int num)
+	{
+		if(num < 0)
+			return false;
+		if(num == 0)
+			return true;
+		long left = 1;
+		long right = (long) Integer.MAX_VALUE;
+		while(left <= right)
+		{
+			long mid = left + right >>> 1;
+			long midSqr = mid * mid;
+			if(midSqr == num)
+				return true;
+			else if(midSqr > num)
+				right = mid - 1;
+			else
+				left = mid + 1;
+		}
+		return false;
+	}
+}
